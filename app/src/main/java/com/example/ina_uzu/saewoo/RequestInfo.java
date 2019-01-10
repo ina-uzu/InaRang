bcf96585285cf4f3365f5e045f5f34654c46419a
ina-uzu <ina.uzu.song@gmail.com>
2019-01-11 00:21:55 +0900

    layout change

--- app/src/main/java/com/example/ina_uzu/saewoo/RequestInfo.java
+++ app/src/main/java/com/example/ina_uzu/saewoo/RequestInfo.java
@@ -1,6 +1,6 @@
 package com.example.ina_uzu.saewoo;
 
 public class RequestInfo {
-    public final static String base_url = "http://4a3812c6.ngrok.io/";
+    public final static String base_url = "http://hellosaewoo.herokuapp.com/";
 
 }
